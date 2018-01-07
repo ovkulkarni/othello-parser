1b45203edc838e781f5a89ceb8953eafbe4b4d1f
ovkulkarni <2019okulkarn@tjhsst.edu>
2018-01-07 12:20:43 -0500

    rename package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
-    name='othello_parser_tjhsst',
-    packages=['othello_parser_tjhsst'],
-    version='1.0.3',
+    name='othello_parser',
+    packages=['othello_parser'],
+    version='1.0.4',
     description='Othello Parser for TJHSST Labs',
     author='Omkar Kulkarni',
     author_email='okulkarni@tjhsst.edu',
     url='https://github.com/ovkulkarni/othello-parser',
-    download_url='https://github.com/ovkulkarni/othello-parser/archive/1.0.tar.gz',
+    download_url='https://github.com/ovkulkarni/othello-parser/archive/1.0.4.tar.gz',
     keywords=['othello', 'argparse', 'tjhsst', 'gabor', 'parser'],
     classifiers=[],
 )
