1f14ee3a86b04bf306aaebb8c465bacd43efc35c
ovkulkarni <2019okulkarn@tjhsst.edu>
2018-01-07 11:52:34 -0500

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='othello_parser_tjhsst',
+    packages=['othello_parser_tjhsst'],
+    version='1.0',
+    description='Othello Parser for TJHSST Labs',
+    author='Omkar Kulkarni',
+    author_email='okulkarni@tjhsst.edu',
+    url='https://github.com/ovkulkarni/othello-parser',
+    download_url='https://github.com/ovkulkarni/othello-parser/archive/1.0.tar.gz',
+    keywords=['othello', 'argparse', 'tjhsst', 'gabor', 'parser'],
+    classifiers=[],
+)
